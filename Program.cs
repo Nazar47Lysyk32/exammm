33928c0b318fd2523b68ce3a5b256ce0aecdd3f3
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2024-04-01 16:01:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ namespace WindowsFormsApp2
 {
     internal static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+ 
         [STAThread]
         static void Main()
         {
